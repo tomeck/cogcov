c7739c9d85a42dc0ba3ca232b7baae1af0083e50
Tom Eck <tomeck@Toms-MBP.home>
2017-02-24 12:32:13 -0500

    integrated Watson Conversation

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-//require the Twilio module and create a REST client
+// For Twilio integration
 var twilio = require('twilio');
+// create a Twilio REST client
 //var client = require('./twilio-node/lib')('ACbb28e740dadc52c2575f545b1d544b83', 'd31a27296b3b9ea2f0893cc64411198b');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+
+// Setup Watson Conversation
+var Conversation = require('watson-developer-cloud/conversation/v1');
+
+// JTE TODO - get from VCAP_SERVICES?
+var conversationUsername = 'e7628040-92ad-4a01-8415-467b78ee3110';
+var conversationPassword = 'qGz4gBDXiHHi';
+var conversationWorkspace = 'ba1d1b65-2ce5-4901-8377-2de214dea244';
+
+// Create the service wrapper
+var conversation = new Conversation({
+  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+  username: conversationUsername,
+  password: conversationPassword,
+  version_date: Conversation.VERSION_DATE_2017_02_03
+  //url: 'https://gateway.watsonplatform.net/conversation/api'
+});
+// End Watson Conversation
 
 var app = express();
 
@@ -25,6 +45,65 @@ app.use( bodyParser.urlencoded({ extended: true }));
 
 // Use this to get the entire body in raw format
 //app.use(bodyParser.text({type: '*/*'}));
+
+app.post('/sms', function(req, res) {
+  /*
+  var workspace = process.env.WORKSPACE_ID || conversationWorkspace;
+  if (!workspace || workspace === conversationWorkspace) {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  */
+
+  console.log(req.headers['content-type']);
+
+  // FORM-URLENCODED as per Twilio POST format
+  var inFrom = req.body.From;
+  var inBody = req.body.Body;
+  console.log('Input received: ' + inBody + ' from ' + inFrom );
+
+
+  //console.log('Workspace id is: ' + conversationWorkspace);
+
+/*
+  var payload = {
+    workspace_id: conversationWorkspace,
+    context: bcontext || {},
+    input: binput || {}
+  };
+*/
+
+  // JTE TODO - do I need to maintain/pass in conversation context?
+  // Send the input to the conversation service
+  conversation.message({
+      input: { text: inBody },
+      workspace_id: conversationWorkspace
+    }, function(err, data) {
+      var responseMsg;
+      if (err) {
+        console.error('Something bad happened: ' + JSON.stringify(err, null, 2));
+        responseMsg = JSON.stringify(err, null, 2)
+      } else {
+        // Extract response
+        var resptext = data.output.text[0];
+        console.log(resptext);
+        responseMsg = JSON.stringify({'response':resptext});
+      }
+
+      // Compose the response to Twilio that will be SMS'd back to originator
+      var twiml = new twilio.TwimlResponse();
+      twiml.message(responseMsg);
+      res.writeHead(200, {'Content-Type': 'text/xml'});
+      res.end(twiml.toString());
+
+      console.log("Wrote message back to Twilio");
+  });
+});
+
+/* --- ORIG WORKING
 
 app.post('/sms', function(req, res) {
 
@@ -57,6 +136,8 @@ app.post('/sms', function(req, res) {
     console.log('ERROR: ' + e);
   }
 });
+
+*/
 
 // JTE NEEDED?
 // get the app environment from Cloud Foundry
